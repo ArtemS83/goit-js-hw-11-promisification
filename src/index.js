e95f25b2625df5b9c0de82a6fcaada1f09e05044
Atrem Sergieiev <a_serg83@ukr.net>
2020-12-17 17:32:58 +0100

    added task-3

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import './styles.css';
 
-//==================Задание 1================//
+//================== ЗАДАНИЕ 1 ================//
 
 // Напиши функцию delay(ms), которая возвращает промис,
 // переходящий в состояние "resolved" через ms миллисекунд.
@@ -22,7 +22,7 @@ delay(2000).then(logger); // Resolved after 2000ms
 delay(1000).then(logger); // Resolved after 1000ms
 delay(1500).then(logger); // Resolved after 1500ms
 
-//==================Задание 2====================//
+//================== ЗАДАНИЕ 2 ====================//
 
 // Перепиши функцию toggleUserState() так,
 // чтобы она не использовала callback - функцию callback,
@@ -35,13 +35,13 @@ const users = [
   { name: 'Lux', active: false },
 ];
 
-const toggleUserState = (allUsers, userName, callback) => {
-  const updatedUsers = allUsers.map(user =>
-    user.name === userName ? { ...user, active: !user.active } : user,
-  );
+// const toggleUserState = (allUsers, userName, callback) => {
+//   const updatedUsers = allUsers.map(user =>
+//     user.name === userName ? { ...user, active: !user.active } : user,
+//   );
 
-  callback(updatedUsers);
-};
+//   callback(updatedUsers);
+// };
 
 const promiseToggleUserState = (allUsers, userName) => {
   const updatedUsers = allUsers.map(user =>
@@ -57,8 +57,8 @@ const logger2 = updatedUsers => console.table(updatedUsers);
 /*
  * Сейчас работает так
  */
-toggleUserState(users, 'Mango', logger2);
-toggleUserState(users, 'Lux', logger2);
+// toggleUserState(users, 'Mango', logger2);
+// toggleUserState(users, 'Lux', logger2);
 
 /*
  * Должно работать так
@@ -66,3 +66,82 @@ toggleUserState(users, 'Lux', logger2);
 promiseToggleUserState(users, 'Mango').then(logger2);
 promiseToggleUserState(users, 'Lux').then(logger2);
 // promiseToggleUserState(users, 'Poly').then(console.table); // так тоже можно
+
+//================= ЗАДАНИЕ 3 ====================//
+
+// Перепиши функцию makeTransaction() так, чтобы она не использовала
+// callback - функции onSuccess и onError,
+// а принимала всего один параметр transaction и возвращала промис.
+
+const randomIntegerFromInterval = (min, max) => {
+  return Math.floor(Math.random() * (max - min + 1) + min);
+};
+/*
+ * Должно работать так
+ */
+const promiseMakeTransaction = transaction => {
+  const delayPromise = randomIntegerFromInterval(200, 500);
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      const canProcess = Math.random() > 0.3;
+      if (canProcess) {
+        const data = { id: transaction.id, time: delayPromise };
+        resolve(data);
+      } else {
+        reject({ id: transaction.id });
+      }
+    }, delayPromise);
+  });
+};
+
+const logSuccess = ({ id, time }) => {
+  console.log(`Transaction ${id} processed in ${time} ms`);
+};
+
+const logError = ({ id }) => {
+  console.warn(`Error processing transaction ${id}. Please try again later.`);
+};
+
+promiseMakeTransaction({ id: 70, amount: 150 })
+  .then(logSuccess)
+  .catch(logError);
+
+promiseMakeTransaction({ id: 71, amount: 230 })
+  .then(logSuccess)
+  .catch(logError);
+
+promiseMakeTransaction({ id: 72, amount: 75 }).then(logSuccess).catch(logError);
+
+promiseMakeTransaction({ id: 73, amount: 100 })
+  .then(logSuccess)
+  .catch(logError);
+/*
+ * Работает так
+ */
+// const makeTransaction = (transaction, onSuccess, onError) => {
+//   const delay = randomIntegerFromInterval(200, 500);
+
+//   setTimeout(() => {
+//     const canProcess = Math.random() > 0.3;
+
+//     if (canProcess) {
+//       onSuccess(transaction.id, delay);
+//     } else {
+//       onError(transaction.id);
+//     }
+//   }, delay);
+// };
+/*
+ * Работает так
+ */
+// const logSuccess = (id, time) => {
+//   console.log(`Transaction ${id} processed in ${time}ms`);
+// };
+
+// const logError = id => {
+//   console.warn(`Error processing transaction ${id}. Please try again later.`);
+// };
+// makeTransaction({ id: 70, amount: 150 }, logSuccess, logError);
+// makeTransaction({ id: 71, amount: 230 }, logSuccess, logError);
+// makeTransaction({ id: 72, amount: 75 }, logSuccess, logError);
+// makeTransaction({ id: 73, amount: 100 }, logSuccess, logError);
